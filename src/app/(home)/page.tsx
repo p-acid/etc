d75a167f2ca24617a08dc7f7bca6aa2bc63dee48
p-acid <qkrtkstjd01@gmail.com>
2025-05-09 21:52:02 +0900

    docs: update key concepts

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,19 +1,25 @@
-import Link from 'next/link';
+import { Metadata } from "next";
+import Link from "next/link";
 
 export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
       <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
       <p className="text-fd-muted-foreground">
-        You can open{' '}
+        You can open{" "}
         <Link
           href="/docs"
           className="text-fd-foreground font-semibold underline"
         >
           /docs
-        </Link>{' '}
+        </Link>{" "}
         and see the documentation.
       </p>
     </main>
   );
 }
+
+export const metadata: Metadata = {
+  title: "etc.",
+  description: "Documents by p-acid",
+};
