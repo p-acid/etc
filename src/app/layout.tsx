c2b3153403bc2a7793b433a3ff2afbfcc032d97d
p-acid <qkrtkstjd01@gmail.com>
2025-06-12 17:29:24 +0900

    style: set pretendard font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,18 @@
 import "./global.css";
 import { RootProvider } from "fumadocs-ui/provider";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import type { ReactNode } from "react";
 
-const inter = Inter({
-  subsets: ["latin"],
+const pretendard = localFont({
+  src: "../fonts/PretendardVariable.woff2",
+  display: "swap",
+  weight: "45 920",
+  variable: "--font-pretendard",
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <html lang="ko" className={inter.className} suppressHydrationWarning>
+    <html lang="ko" className={pretendard.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
