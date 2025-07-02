7739980fcd4f706eec22d705ee6b46861a88ba69
p-acid <qkrtkstjd01@gmail.com>
2025-07-02 13:13:34 +0900

    style: layout style

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -1,11 +1,12 @@
-import { DocsLayout } from 'fumadocs-ui/layouts/docs';
-import type { ReactNode } from 'react';
-import { baseOptions } from '@/app/layout.config';
-import { source } from '@/lib/source';
+import { DocsLayout } from "fumadocs-ui/layouts/notebook";
+import type { ReactNode } from "react";
+import { baseOptions } from "@/app/layout.config";
+import { source } from "@/lib/source";
+import { GITHUB_URL } from "@/consts/urls";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <DocsLayout tree={source.pageTree} {...baseOptions}>
+    <DocsLayout tree={source.pageTree} githubUrl={GITHUB_URL} {...baseOptions}>
       {children}
     </DocsLayout>
   );
